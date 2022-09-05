d7546fe68c24474ede172886edd85fe3693b81b1
Josh May <joshmay222@gmail.com>
2022-09-05 16:06:21 -0500

    adding calc widget

--- dev/src/views/message.js
+++ dev/src/views/message.js
@@ -1,21 +1,96 @@
 import html from './message.html';
 import './message.css';
 
-let elements = [];
 let body;
+let tmp = document.createElement('div');
+tmp.innerHTML = html;
+let container = tmp.querySelector('.tlgc');
+let elements = [];
 
-export function show(text) {
-  // convert plain HTML string into DOM elements
+/* Initial configuration for the widget */
+export function config(config) {
+  // Add animations if the option is enabled
+
   let temporary = document.createElement('div');
   temporary.innerHTML = html;
-  //   temporary.getElementsByClassName('js-widget-dialog')[0].textContent = text;
 
-  // append elements to body
+  if (config.useAnimations) {
+    container.querySelector('.tlgm').classList.add('slide');
+    container.querySelector('.telewidget-button').classList.add('zoomIn');
+  }
+
   body = document.getElementsByTagName('body')[0];
   while (temporary.children.length > 0) {
     elements.push(temporary.children[0]);
     body.appendChild(temporary.children[0]);
   }
+
+  // Add dark theme classes if the option is enabled
+  if (config.useDarkTheme) {
+    container.classList.add('dark');
+  }
+
+  // Welcome message
+  if (config.message) {
+    // If we use cookies, check to see if the cookie exists already
+    if (
+      config.useCookies &&
+      document.cookie.match(/^(.*;)?\s*showmessage\s*=\s*[^;]+(.*)?$/) == null
+    ) {
+      container.querySelector(
+        '.tlgm'
+      ).innerHTML = `${config.message} <span></span>`;
+      // Event listener to close the message on the widget
+      container
+        .querySelector('.tlgm span')
+        .addEventListener('click', closeMessage);
+      // Create the cookie
+      createCookie('showmessage', 'no', 1);
+      // Show the message if we're not using cookies
+    } else if (!config.useCookies) {
+      container.querySelector(
+        '.tlgm'
+      ).innerHTML = `${config.message} <span></span>`;
+      // Event listener to close the message on the widget
+      container
+        .querySelector('.tlgm span')
+        .addEventListener('click', closeMessage);
+      // Remove the div that contains the welcome message if we're using the cookie and it already exists
+    } else {
+      container.querySelector('.tlgm').remove();
+    }
+    // If there's no welcome message, remove that div
+  } else {
+    container.querySelector('.tlgm').remove();
+  }
+
+  // Add telegram link
+  if (config.users) {
+    try {
+      let randomUsers =
+        config.users[Math.floor(Math.random() * config.users.length)];
+
+      container.querySelector(
+        '.telewidget-button'
+      ).href = `https://t.me/${randomUsers}`;
+    } catch (error) {
+      console.error('telewidget -> cannot select users');
+    }
+  }
+
+  // Show the widget on the left side of the page
+  if (config.floatLeft) {
+    container.classList.add('left');
+  }
+
+  // Show the widget message on mobile devices
+  if (config.showMessageAlways) {
+    container.classList.add('show-message-always');
+  }
+
+  // Add the widget to the body of your page
+  body = document.getElementsByTagName('body')[0];
+  body.appendChild(container);
 
   class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
@@ -157,9 +232,26 @@ export function show(text) {
   body.addEventListener('click', close);
 }
 
-// export function close() {
-//   while (elements.length > 0) {
-//     elements.pop().remove();
-//   }
-//   //   body.removeEventListener('click', close);
-// }
+/* Close widget welcome message */
+export function closeMessage() {
+  container.querySelector('.tlgm').style.display = 'none';
+}
+
+/* Function to create a cookie to show the message only once every day */
+/* Taken from here https://stackoverflow.com/questions/6561687/how-can-i-set-a-cookie-to-expire-after-x-days-with-this-code-i-have */
+function createCookie(name, value, days) {
+  console.log('creating cookie');
+
+  let date;
+  let expires;
+
+  if (days) {
+    date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = '; expires=' + date.toGMTString();
+  } else {
+    expires = '';
+  }
+
+  document.cookie = name + '=' + value + expires + '; path=/';
+}
